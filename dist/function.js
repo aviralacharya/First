b98be4cb9d97b5dca28d01da2c5b76873a9ac3af
Abiral Acharya <abiralacharya80@gmail.com>
2025-05-02 22:40:12 +0545

    5/2

--- dist/function.js
+++ dist/function.js
@@ -1,5 +1,15 @@
 "use strict";
-function calculateTax(income) {
-    return 0;
-}
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline_1 = __importDefault(require("readline"));
+const rl = readline_1.default.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question('Enter your name: ', (name) => {
+    console.log(`Hello, ${name}!`);
+    rl.close();
+});
 //# sourceMappingURL=function.js.map
--- src/function.ts
+++ src/function.ts
@@ -1,3 +1,11 @@
-function calculateTax(income: number): number{
-    return 0;
-}
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Enter your name: ', (name) => {
+  console.log(`Hello, ${name}!`);
+  rl.close();
+});
